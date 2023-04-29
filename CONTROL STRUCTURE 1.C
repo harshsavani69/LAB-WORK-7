efe8aaca1f22920f91764317ec80714ada895d8e
harsh savani <131761988+harshsavani69@users.noreply.github.com>
2023-04-29 11:42:38 +0530

    Add files via upload

---
+++ CONTROL STRUCTURE 1.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+   clrscr();
+   int neutral;
+   printf("enter neutral");
+   scanf("%d",&neutral);
+   if(neutral>1 0)
+   {
+     printf("the number is positive");
+   }
+     else if (neutral==00)
+   {
+      printf("the number is neutral");
+   }
+      else if (neutral<-10)
+   {
+       printf("the number is nagative");
+   }
+
+   getch();
+}
---
+++ CONTROL STRUCTUTE 2.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+
+   clrscr();
+   int a=10,b=50;
+   if(b<a)
+   {
+      printf("a is minimum");
+   }
+   else
+   {
+      printf("b is minimum");
+   }
+   getch();
+
+}
